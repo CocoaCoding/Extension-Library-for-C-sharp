1ecd28a1488e0c67a46ef01da54ab1d85c8554f6
Holger Hinzberg <hinzberg@outlook.com>
2020-02-19 11:34:26 +0100

    Initial commit

---
+++ ExtentionLibrary/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace ExtensionsLibrary
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+    
+        }
+    }
+}
